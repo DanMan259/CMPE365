86a8136b71401e38f4cea89c6542d9917266f194
Daniyal Maniar <16mdm13@caslab.queensu.ca>
2019-10-11 21:43:19 -0400

    Lab 4/5 Bug Fixes

--- Lab4and5/modifiedSolution.py
+++ Lab4and5/modifiedSolution.py
@@ -3,31 +3,35 @@
 """
 Created on Sun Sep 29 23:45:39 2019
 
-@author: daniyal
+@author: Daniyal Maniar
+
+I certify that this submission contains my own work. 
+The algorithms were based upon the pseudo code provided in the definition of the Lab.
+
+This file reflects the same algorithms but they are modified to count operations instead of results. 
+This will require matplotlib to be installed
+
 """
-import matplotlib.pyplot as plt
+
 import random
 import math
+# Change this line to zero if you dont have matplotlib to plot the results
+PLOT = 1
 
 def createSet_OBJ(array):
-    return {"set": array, "sum": sum(array)}
+    return {"set": array[:], "sum": sum(array)}
 
 def BFI_Subset_Sum(S, k):
     count = 0
     subsets = []
     subsets.append(createSet_OBJ([]))
-    count += 2
     for value in S["set"]:
-        count += 1
         newSubsets = []
         for subset in subsets:
-            count += 1
             newSubsets.append(createSet_OBJ(subset["set"]+[value]))
-            count += 2
-            if newSubsets[-1]["sum"] ==  k:
-                count += 1 
-                return count
             count += 1
+            if newSubsets[-1]["sum"] ==  k:
+                return count
         subsets += newSubsets
         count += 1
     return count
@@ -46,35 +50,30 @@ def BFI_Subset_Sum_Modified_Count(S):
     count = 0
     subsets = []
     subsets.append(createSet_OBJ([]))
-    count += 2
     for value in S["set"]:
-        count += 1
         newSubsets = []
         for subset in subsets:
-            count += 1
             newSubsets.append(createSet_OBJ(subset["set"]+[value]))
-            count += 2
+            count += 1
         subsets += newSubsets
         count += 1
     return count
 
 def HS_Subset_Sum(S, k):
     count = 0
-    S_left = BFI_Subset_Sum_Modified(createSet_OBJ(S["set"][:len(S)//2]))
-    count += 2
-    count += BFI_Subset_Sum_Modified_Count(createSet_OBJ(S["set"][:len(S)//2]))
-    S_right = BFI_Subset_Sum_Modified(createSet_OBJ(S["set"][len(S)//2:]))
-    count += 2
-    count += BFI_Subset_Sum_Modified_Count(createSet_OBJ(S["set"][len(S)//2:]))
+    S_left = BFI_Subset_Sum_Modified(createSet_OBJ(S["set"][:len(S["set"])//2]))
+    count += 1
+    count += BFI_Subset_Sum_Modified_Count(createSet_OBJ(S["set"][:len(S["set"])//2]))
+    S_right = BFI_Subset_Sum_Modified(createSet_OBJ(S["set"][len(S["set"])//2:]))
+    count += 1
+    count += BFI_Subset_Sum_Modified_Count(createSet_OBJ(S["set"][len(S["set"])//2:]))
     for obj in S_left:
-        count += 2
+        count += 1
         if obj['sum'] == k:
-            count += 1
             return count
     for obj in S_right:
-        count += 2
+        count += 1
         if obj['sum'] == k:
-            count += 1
             return count
     S_left.sort(key=lambda x:x["sum"])
     count += 3 * len(S_left) * math.log2(len(S_left))
@@ -83,9 +82,7 @@ def HS_Subset_Sum(S, k):
     Pointers = Pair_Sum(S_left, S_right, k)
     count += Pair_Sum_Count(S_left, S_right, k)
     if Pointers:
-        count += 1
         return count
-    count += 1
     return count
 
 def Pair_Sum(Values_1, Values_2, k):
@@ -107,54 +104,45 @@ def Pair_Sum_Count(Values_1, Values_2, k):
     p2 = len(Values_2)-1
     while (p1 < len(Values_1) and p2 >= 0):
         t = Values_1[p1]["sum"]+Values_2[p2]["sum"]
-        count += 1
+        count += 2
         if (t==k):
-            count += 1
             return count
         elif t < k:
             p1 += 1
         else:
             p2 -= 1
-        count += 2
     return count
 
 if __name__ == '__main__':
+    # This section calculates the averages
     bfiSetAverages= []
     hsSetAverages= []
     for n in range(4, 16):
-        bfssAverage = 0
-        hsssAverage = 0
-        for i in range (1, 21):
-            array = [random.randint(1, 300) for _ in range(n)]
-            S = createSet_OBJ(array)
-            kSet = [random.randint(1, 700) for _ in range(10)]
-            bfssSum = 0
-            hsssSum = 0
+        bfNAverage = 0
+        hsNAverage = 0
+        for _ in range (1, 21):
+            S = createSet_OBJ([random.randint(1, 1000) for __ in range(n)])
+            kSet = [random.randint(500, 1500) for __ in range(20)]
+            bfTSum = 0
+            hsTSum = 0
             for k in kSet:
-                bfssSum += BFI_Subset_Sum(S, k)
-                hsssSum += HS_Subset_Sum(S, k)   
-            bfssAverage += bfssSum / len(kSet)
-            hsssAverage += hsssSum / len(kSet)
-        bfiSetAverages.append(bfssAverage/n)
-        hsSetAverages.append(hsssAverage/n)
+                bfTSum += BFI_Subset_Sum(S, k)
+                hsTSum += HS_Subset_Sum(S, k)   
+            bfNAverage += bfTSum / len(kSet)
+            hsNAverage += hsTSum / len(kSet)
+        bfiSetAverages.append(bfNAverage/n)
+        hsSetAverages.append(hsNAverage/n)
     
-    
-    
-    # X axis values
-    x = list(range(4, 16)) 
-    # plotting the points
-    plt.plot(x, bfiSetAverages, label="BFI")
-    plt.plot(x, hsSetAverages, label="Horowitz Sahni") 
-    # plot the mathematical models to compare with
-    plt.plot(x, list(map(lambda n: 2 ** n, x)), label="2^n")
-    plt.plot(x, list(map(lambda n: n * (2 ** (n // 2)), x)), label="n*2^(n/2)") 
-    # display the legend
-    plt.legend()
-    # naming the x axis
-    plt.xlabel('Sets')
-    # naming the y axis
-    plt.ylabel('Number of Operations') 
-    # giving a title to my graph
-    plt.title('Subset Sum algorithm analysis') 
-    # function to show the plot
-    plt.show()
+    # This section plots the results
+    if PLOT:
+        import matplotlib.pyplot as plt
+        x_axis = list(range(4, 16)) 
+        plt.plot(x_axis, bfiSetAverages, label="BFI")
+        plt.plot(x_axis, list(map(lambda n: 2 ** n, x_axis)), label="2^n")
+        plt.plot(x_axis, hsSetAverages, label="Horowitz Sahni") 
+        plt.plot(x_axis, list(map(lambda n: 5*n * (2 ** (n / 2)), x_axis)), label="5*n*2^(n/2)") 
+        plt.legend()
+        plt.xlabel('Sets')
+        plt.ylabel('Number of Operations') 
+        plt.title('Subset Sum algorithm analysis') 
+        plt.show()
--- Lab4and5/solution.py
+++ Lab4and5/solution.py
@@ -3,12 +3,18 @@
 """
 Created on Sun Sep 29 20:24:50 2019
 
-@author: daniyal
-"""
+@author: Daniyal Maniar
 
+I certify that this submission contains my own work. 
+The algorithms were based upon the pseudo code provided in the definition of the Lab.
+
+This file has the solution in a non-modified format
+"""
+# This makes a create object that we work with
 def createSet_OBJ(array):
     return {"set": array[:], "sum": sum(array)}
 
+# This is the BFI Subset Sum Algorithm
 def BFI_Subset_Sum(S, k):
     subsets = []
     subsets.append(createSet_OBJ([]))
@@ -20,7 +26,7 @@ def BFI_Subset_Sum(S, k):
                 return True
         subsets += newSubsets
     return False
-
+# This is the modified BFI Subset Sum Algorithm
 def BFI_Subset_Sum_Modified(S):
     subsets = []
     subsets.append(createSet_OBJ([]))
@@ -30,10 +36,11 @@ def BFI_Subset_Sum_Modified(S):
             newSubsets.append(createSet_OBJ(subset["set"]+[value]))
         subsets += newSubsets
     return subsets
-            
+
+# This is the HS Subset Sum Algorithm
 def HS_Subset_Sum(S, k):
-    S_left = BFI_Subset_Sum_Modified(createSet_OBJ(S["set"][:len(S)//2]))
-    S_right = BFI_Subset_Sum_Modified(createSet_OBJ(S["set"][len(S)//2:]))
+    S_left = BFI_Subset_Sum_Modified(createSet_OBJ(S["set"][:len(S["set"])//2]))
+    S_right = BFI_Subset_Sum_Modified(createSet_OBJ(S["set"][len(S["set"])//2:]))
     for obj in S_left:
         if obj['sum'] == k:
             return obj['set']
@@ -47,6 +54,7 @@ def HS_Subset_Sum(S, k):
         return S_left[Pointers[0]]["set"]+ S_right[Pointers[1]]["set"]
     return []
 
+# This is the Pair Sum Algorithm
 def Pair_Sum(Values_1, Values_2, k):
     p1 = 1 
     p2 = len(Values_2)-1
@@ -62,12 +70,12 @@ def Pair_Sum(Values_1, Values_2, k):
 
 if __name__ == '__main__':
     # Case 1
-    array =  [2,5,9,3,6,8,7,4,5,1]
+    array =  [2,5,6,7,3,4,8,1,9]
     print("Use this array:")
     print(array)
     S = createSet_OBJ(array)
     print ("To achieve:")
-    k = 16
+    k = 19
     print (k)
     if (BFI_Subset_Sum(S, k)):
         result = HS_Subset_Sum(S, k)
