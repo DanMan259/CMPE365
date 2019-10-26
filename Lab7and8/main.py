6a134de03c8560a504fa96b2bce75ba5915ae39f
Daniyal Maniar <16mdm13@caslab.queensu.ca>
2019-10-25 23:52:37 -0400

    Lab7and8 Start

---
+++ Lab7and8/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 16:58:05 2019
+
+@author: Daniyal Maniar
+
+I certify that this submission contains my own work. 
+The algorithms were based upon the pseudo code provided in the definition of the Lab.
+
+This file has the solution in a non-modified format
+"""
+
+
+
+if __name__ == '__main__':
+    x=1
